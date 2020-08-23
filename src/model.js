7541101cab3e5155e726411003eb8f467514d4bb
Vladislav Podkaura <tripledoublehs@ukr.net>
2020-08-23 14:18:31 +0300

    Content model added

---
+++ src/model.js
@@ -0,0 +1,5 @@
+export const model = [
+    {type: 'title', value: 'Test Title', options: {}},
+    {type: 'text', value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', options: {}},
+    {type: 'textColumns', value: ['text1', 'text2', 'text3'], options: {}}
+]
