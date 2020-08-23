63294d97d00980192572e399867d30f64cfa3caa
Vladislav Podkaura <tripledoublehs@ukr.net>
2020-08-23 20:42:58 +0300

    OOP and SOLID implemented

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
-export function row(content) {
-    return `<div class='row'>${content}</div>`
-}
-
-export function col(content) {
-    return `<div class='col-sm'>${content}</div>`
-}
+export function row(content, styles = '') {
+    const style = `margin: 0; ${styles}`
+    return `<div class="row" style="${style}">${content}</div>`
+  }
+  
+  export function col(content) {
+    return `<div class="col-sm">${content}</div>`
+  }
